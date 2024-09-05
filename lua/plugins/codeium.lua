f17537820b130be6978cbbffdbb6e2edbba58cda
Med Amine Slama <mohammedaminslama@gmail.com>
2024-09-05 02:54:34 +0100

    make codeium not a bitch and easy to use

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,4 +1,20 @@
 return {
-  'Exafunction/codeium.vim',
-  event = 'BufEnter'
+  "Exafunction/codeium.vim",
+  event = "BufEnter",
+  config = function()
+    -- Change '<C-g>' here to any keycode you like.
+    vim.g.codeium_disable_bindings = 1
+    vim.keymap.set("i", "<A-a>", function()
+      return vim.fn["codeium#Accept"]()
+    end, { expr = true, silent = true })
+    vim.keymap.set("i", "<A-k>", function()
+      return vim.fn["codeium#CycleCompletions"](1)
+    end, { expr = true, silent = true })
+    vim.keymap.set("i", "<A-j>", function()
+      return vim.fn["codeium#CycleCompletions"](-1)
+    end, { expr = true, silent = true })
+    vim.keymap.set("i", "<A-c>", function()
+      return vim.fn["codeium#Clear"]()
+    end, { expr = true, silent = true })
+  end,
 }
