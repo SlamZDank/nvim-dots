c673499662d5f830090904ccb0ff0a7d0a1aa271
Med Amine Slama <mohammedaminslama@gmail.com>
2024-12-21 10:02:32 +0100

    add somewindow saving thing with nvchad

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -128,7 +128,7 @@ local options = {
   },
 
   term = {
-    winopts = { number = true, relativenumber = true },
+    winopts = { number = true, relativenumber = true, winfixbuf = true },
     sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
     float = {
       relative = "editor",
