07be1bd16946b7e58cd9905a28694109b9bc19e7
SlamZDank <mohammedaminslama@gmail.com>
2025-08-25 17:42:09 +0100

    change some commands

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -9,14 +9,17 @@ return {
 
       local keymap = vim.keymap.set
 
-      keymap("n", "<Leader>dt", dap.toggle_breakpoint, { desc = "Toggle Debugger Breakpoint" })
       keymap("n", "<Leader>do", dap.continue, { desc = "Start or continue the Debugger" })
+      keymap("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle Debugger Breakpoint" })
+      keymap("n", "<Leader>dcb", dap.clear_breakpoints, { desc = "Clear Debugger Breakpoints" })
       keymap("n", "<Leader>dx", dap.terminate, { desc = "Kill the Debugger" })
 
+      keymap("n", "<Leader>dr", dap.restart, { desc = "Restart the Debugger" })
+
+      keymap("n", "<Leader>dl", dap.run_last, { desc = "Run Last" })
       keymap("n", "<Leader>dso", dap.step_over, { desc = "Step Over" })
       keymap("n", "<Leader>dsi", dap.step_into, { desc = "Step Into" })
       keymap("n", "<Leader>dsu", dap.step_out, { desc = "Step Out" })
-      keymap("n", "<Leader>dl", dap.run_last, { desc = "Run Last" })
 
       keymap("n", "<Leader>duo", dapui.open, { desc = "Open the DebuggerUI" })
       keymap("n", "<Leader>dux", dapui.close, { desc = "Close the DebuggerUI" })
