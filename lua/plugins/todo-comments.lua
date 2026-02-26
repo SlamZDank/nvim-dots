758eaa8c0eb50525a89d8b77e0e86c26e5dc8c7b
SlamZDank <mohammedaminslama@gmail.com>
2026-02-26 22:06:49 +0100

    fix leader tt

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -10,19 +10,19 @@ return {
         alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
         -- signs = false, -- configure signs for some keywords individually
       },
-      TODO = { icon = " ", color = "info" },
-      HACK = { icon = " ", color = "warning" },
+      TODO = { icon = " ", color = "info" },
+      HACK = { icon = "󱏚 ", color = "warning" },
       WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-      PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-      NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-      TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+      PERF = { icon = "󱡮 ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      NOTE = { icon = "󰠮 ", color = "hint", alt = { "INFO" } },
+      TEST = { icon = "󰙨 ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
     }
 
     --see: 
     -- Custom keywords
     local keywords = {
       SEE = {
-        icon = "",
+        icon = " ",
         color = "info",
       },
     }
