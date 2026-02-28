ebbef669ad86825a02fd172dab4c47489e0277f2
SlamZDank <mohammedaminslama@gmail.com>
2026-02-28 16:48:57 +0100

    big update

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -1,11 +1,12 @@
 return {
   "saecki/crates.nvim",
+  event = { "BufRead Cargo.toml" },
   ft = { "toml" },
   config = function()
     require("crates").setup {
       lsp = {
         enabled = true,
-        on_attach = require("configs.lspconfig").on_attach,
+        on_attach = require("nvchad.configs.lspconfig").on_attach,
         completion = true,
         actions = true,
       },
