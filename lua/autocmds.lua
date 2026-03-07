491be29961d6e5955c6916d755c2ad8e793f7616
SlamZDank <mohammedaminslama@gmail.com>
2026-03-07 09:29:12 +0100

    make the indent limit aligned with intellij limits

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -26,12 +26,13 @@ vim.api.nvim_create_autocmd({ "BufEnter", "TextChanged", "TextChangedI", "LspAtt
 
     for i = 0, vim.api.nvim_buf_line_count(bufnr) - 1 do
       local line = vim.api.nvim_buf_get_lines(bufnr, i, i + 1, false)[1]
-      if #line > 80 then
+      local indent_limit = 120
+      if #line > indent_limit then
         table.insert(diagnostics, {
           lnum = i,
-          col = 80,
+          col = indent_limit,
           end_col = #line,
-          message = "Line is too long, please wrap it!\nLength: " .. #line .. " > 80",
+          message = "Line is too long, please wrap it!\nLength: " .. #line .. " > " .. indent_limit,
           severity = vim.diagnostic.severity.WARN,
           source = "linelen",
         })
